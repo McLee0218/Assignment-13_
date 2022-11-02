8a8ef3782a9a44b820da9630b7bc2c76ba5d1e54
tamekamclean <tamekasmclean@gmail.com>
2022-11-02 19:53:26 -0400

    added get, post & delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,21 +1,42 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const fs = require('fs');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  
-  // find all categories
+  const allCategories = category.findAll ({
+    include: [{
+      module: Product,
+      attributes: ['product_id']
+    }]
+  })
+  allCategories()
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+  try {
+    res.render('catagory_id')
+  } catch (err) {
+    res.status(400).json(err);
+  }});
 
-router.post('/', (req, res) => {
+  // find one category by its `id` value
+
+  // be sure to include its associated Product;
+
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+let newCategory = await Category.create({
+  item_type: req.body.item_type,
+}
+);
+res.status(200).json(newCategory);
+     } catch (err) {
+         res.status(500).json(err) 
+     }
 });
 
 router.put('/:id', (req, res) => {
@@ -24,6 +45,10 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  let product = JSON.parse(fs.readFileSync('./models/category.js/'));
+  let removeProduct = product.filter(item => item.id !== req.params.id);
+  fs.writeFileSync('./models/category.js/', JSON.stringify(product))
+  res.json(removeProduct);
 });
 
 module.exports = router;

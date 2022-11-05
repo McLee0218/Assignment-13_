b87c541b6505fef0c1392a5b1b851b2d473c0b07
tamekamclean <tamekasmclean@gmail.com>
2022-11-05 11:00:20 -0400

    category & producer routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,50 +5,54 @@ const fs = require('fs');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  const allCategories = category.findAll ({
-    include: [{
-      module: Product,
-      attributes: ['product_id']
-    }]
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
   })
-  allCategories()
+  .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
-  try {
-    res.render('catagory_id')
-  } catch (err) {
-    res.status(400).json(err);
-  }});
+// router.get('/:id', (req, res) => {
+//   try {
+//     res.render('catagory_id')
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }});
 
-  // find one category by its `id` value
+//   // find one category by its `id` value
 
-  // be sure to include its associated Product;
+//   // be sure to include its associated Product;
 
 router.post('/', async (req, res) => {
   // create a new category
-  try {
-let newCategory = await Category.create({
-  item_type: req.body.item_type,
-}
-);
-res.status(200).json(newCategory);
-     } catch (err) {
-         res.status(500).json(err) 
-     }
+Category.create({
+  category_name: req.body.category_name,
+}).then(categoryData => res.json(categoryData))
+.catch(err => {
+  res.status(500).json(err);
+});
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-  let product = JSON.parse(fs.readFileSync('./models/category.js/'));
-  let removeProduct = product.filter(item => item.id !== req.params.id);
-  fs.writeFileSync('./models/category.js/', JSON.stringify(product))
-  res.json(removeProduct);
-});
+// router.put('/:id', (req, res) => {
+//   // update a category by its `id` value
+// });
+
+// router.delete('/:id', (req, res) => {
+//   // delete a category by its `id` value
+//   let product = JSON.parse(fs.readFileSync('./models/category.js/'));
+//   let removeProduct = product.filter(item => item.id !== req.params.id);
+//   fs.writeFileSync('./models/category.js/', JSON.stringify(product))
+//   res.json(removeProduct);
+// });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,6 +6,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name'],
+        model: Tag, as: 'product_tags',
+        attributes: ['tag_name']
+      }
+    ]
+  })
+  .then(productData => res.json(productData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Category and Tag data
 });
 
